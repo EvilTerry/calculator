2f91dabe2804ac68446fa51ba1efe2577763e6a3
EvilTerry <91133213+EvilTerry@users.noreply.github.com>
2023-01-18 22:22:23 +0200

    Add add operation

--- javascript.js
+++ javascript.js
@@ -1,3 +1,31 @@
+let displayNumber = 0;
+let calculatedNumber = 0;
+let currentOperator = 'none';
+
+
+const screen = document.querySelector('.screen');
+const clear = document.querySelector('.clear');
+const allClear = document.querySelector('.allClear');
+const operatorBtn = document.querySelector('.operator');
+const addBtn = document.querySelector('.add');
+const subtractBtn = document.querySelector('.subtract');
+const multiplyBtn = document.querySelector('.multiply');
+const divideBtn = document.querySelector('.divide');
+const numberBtns = document.querySelectorAll('.btn');
+
+clear.addEventListener('click', screenClear);
+allClear.addEventListener('click', screenAllClear);
+addBtn.addEventListener('click', addNumbers);
+numberBtns.forEach(button => {
+    button.addEventListener('click', () => { screenEnter(button.textContent) } );
+});
+
+
+
+
+
+
+
 function add(x, y) {
     return x+y
 }
@@ -12,4 +40,35 @@ function multiply(x, y) {
 
 function divide(x, y) {
     return x/y;
+}
+
+function screenEnter(number) {
+    screen.textContent = displayNumber;
+    if(screen.textContent === '0')
+        screen.textContent = number;
+    else
+        screen.textContent += number;
+    displayNumber = screen.textContent;
+}
+
+function screenClear() {
+    if(screen.textContent.length === 1)
+        screen.textContent = 0;
+    else
+        screen.textContent = screen.textContent.substring(0, screen.textContent.length-1);
+    displayNumber = screen.textContent;
+}
+
+function screenAllClear() {
+    screen.textContent = 0;
+}
+
+function operate(operator, firstNumber, secondNumber) {
+
+}
+
+function addNumbers() {
+    calculatedNumber += Number(displayNumber);
+    displayNumber = 0;
+    screen.textContent = calculatedNumber;
 }
